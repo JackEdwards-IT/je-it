b4bbd183208cd58d493e10ae2489a18fb770ead7
JackEdwards-IT <jack@je-it.com>
2021-03-18 17:13:58 +1100

    Adding recaptcha

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,43 +9,64 @@
 
       <b-row class="form-group"></b-row>
       <b-row>
-      
         <b-col cols="2"></b-col>
-      
+
         <b-col cols="8">
+          <form :model="form" @submit.prevent="submit" ref="contactForm">
+            <div class="form-group row">
+              <label></label>
+              <div class="col-sm-10">
+              
+              </div>
+            </div>
+            <div class="form-group row">...</div>
+            <div class="form-group row">
+              <label for="robot" class="col-sm-2 col-form-label"></label>
+              <div class="col-sm-10">
+                <vue-recaptcha
+                  ref="recaptcha"
+                  @verify="onVerify"
+                  sitekey="6LexGYQaAAAAAE8UsVFh8COeUN1GNCmDNPXqP0uL"
+                >
+                </vue-recaptcha>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">Send</button>.
+          </form>
+        </b-col>
 
-      
-          <h2 class="h2-responsive">
-            <form action="https://formspree.io/f/mjvpnqep" method="POST">
-              <div class="g-recaptcha" data-sitekey="6LexGYQaAAAAAE8UsVFh8COeUN1GNCmDNPXqP0uL"></div>
-              <label>
-                Your email:
-                <input type="email" name="_replyto" />
-              </label>
-              <label>
-                Your message:
-                <textarea name="message"></textarea>
-              </label>
-
-              <!-- your other form fields go here -->
-
-              <button type="submit">Send</button>
-            </form>
-            
-          </h2></b-col>
-
-          <b-col cols="2"></b-col>
-
+        <b-col cols="2"></b-col>
       </b-row>
     </b-container>
   </div>
 </template>
 
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
 <script>
-export default {};
+import VueRecaptcha from "vue-recaptcha";
+
+export default {
+  data() {
+    return {
+      form: {
+        robot: false
+      }
+    }
+  },
+  methods: {
+    submit: function() {
+      if (this.form.robot) {
+        console.log("test");
+      }
+    },
+    onVerify: function (response) {
+      if (response) this.form.robot = true;
+    },
+  },
+  components: { VueRecaptcha },
+};
 </script>
+
 <style scoped>
 .h1-responsive {
   text-align: center;
