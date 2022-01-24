60f6d48013ced21903e4908abe013f2ee2d095b3
Jack Edwards <jack@je-it.com>
2022-01-25 08:30:45 +1100

    Fixed contact form, emailjs package name had changed

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,17 +3,11 @@
     <b-container>
       <b-row>
         <div class="mx-auto">
-        <h1>Contact</h1>
+          <h1>Contact</h1>
         </div>
       </b-row>
 
       <b-row>
-       <div class="mx-auto">
-          <p class="p-responsive">Please email me at: <font color=#35a7ff>Jack@je-it.com</font></p>
-       </div>      
-      </b-row>
-
-      <!-- <b-row>
         <b-col cols="0" md="2"></b-col>
 
         <b-col cols="12" md="8">
@@ -63,17 +57,21 @@
                 max-rows="7"
               ></b-form-textarea>
             </b-form-group>
-           
 
-            TODO remove hardcoding of submit state to reflect response from emailJS API 
+            <!-- TODO remove hardcoding of submit state to reflect response from emailJS API  -->
 
-            <b-button :disabled="submitDisabled" @click="changeStates(true)" @clik="sendEmail()" class="custom-btn" type="submit" value="Send"
-              >Send</b-button
+            <b-button
+              :disabled="submitDisabled"
+              
+              @click="sendEmail()"
+              class="custom-btn"
+              type="submit"
+              value="Send"
+              >Send
+              <b-spinner small v-show="sending"></b-spinner></b-button
             >
-
- 
           </b-form>
-          Indicate success or failure of form submission
+          <!-- Indicate success or failure of form submission -->
           <div
             class="alert alert-success mt-2"
             role="alert"
@@ -87,20 +85,19 @@
             role="alert"
             v-show="messageNotSent"
           >
-            Something went wrong. Please contact me directly: jack at je-it dot
-            com.
+            Something went wrong. Please contact me directly: Jack@je-it.com
           </div>
         </b-col>
 
         <b-col cols="0" md="2"></b-col>
-      </b-row> --> 
+      </b-row>
     </b-container>
   </div>
 </template>
 
 
 <script>
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 export default {
   data() {
@@ -108,30 +105,44 @@ export default {
       submitDisabled: false,
       messageNotSent: false,
       messageSent: false,
+      sending: false
     };
   },
   methods: {
     sendEmail() {
-      console.log("send email...")
-      emailjs.sendForm('service_3jfiw2l', 'template_d9zorsq', this.$refs.form, 'user_ToFE396KP3LoLIYBpk92K')
-        .then((result) => {
-            console.log('SUCCESS!', result.text);
-        }, (error) => {
-            console.log('FAILED...', error.text);
-        });
+      this.sending = true
+      emailjs
+        .sendForm(
+          "service_3jfiw2l",
+          "template_d9zorsq",
+          this.$refs.form,
+          "user_ToFE396KP3LoLIYBpk92K"
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.text);
+            this.messageSent = true
+            this.sending = false
+            this.submitDisabled = true
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+            this.messageNotSent = true
+            this.sending = false
+            this.submitDisabled = true
+          }
+        );
     },
-    
-    changeStates(outcome){
-      if(outcome){
+
+    changeStates(outcome) {
+      if (outcome) {
         this.messageSent = true;
         this.submitDisabled = true;
       } else {
         this.messageNotSent = true;
         this.submitDisabled = true;
       }
-
     },
-
   },
 };
 </script>
@@ -148,5 +159,4 @@ export default {
 p {
   margin-bottom: 0;
 }
-
 </style>
